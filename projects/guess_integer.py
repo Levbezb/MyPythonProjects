63ecfdd34c4176dcba542a1475ea77bee5666d8c
Lev <bezborodovlev@gmail.com>
2025-09-29 17:12:50 +0300

    features of project

--- projects/guess_integer.py
+++ projects/guess_integer.py
@@ -1,27 +1,63 @@
 from random import *
 
-n = randint(1, 100)
+rng_numbers = 100
+cnt = 0
 
-print('Добро пожаловать в числовую угадайку')
+def make_range():               # Функция отвечает за указание правой границы, если пользователь захочет выставить свой диапазон чисел
+    while True:
+        global rng_numbers
+        rng_numbers = input('Укажите правую границу: ')
+        if rng_numbers.isdigit():
+            rng_numbers = int(rng_numbers)
+            break
+        else:
+            print('А может быть все-таки введем целое число?')
 
-def is_valid(number):
-    if  number.isdigit() and 1 <= int(number) <= 100:
+
+print('Добро пожаловать в числовую угадайку!')
+
+rng = input('Хотите выбрать диапазон чисел угадайки? Если да, введите "д", если нет, введите "н": ')
+if rng == 'д':
+    make_range()
+
+n = randint(1, rng_numbers)
+
+def is_valid(number):                                           # Функция проверяет число на то, что оно действительно число и лежит в 
+    if  number.isdigit() and 1 <= int(number) <= rng_numbers:   # диапазоне от 1 до указанной правой границы
         return True
     else:
         return False
 
 while True:
-    num = input('Введите число от 1 до 100: ')
+    num = input(f'Введите число от 1 до {rng_numbers}: ')
+
     if is_valid(num):
         num = int(num)
     else:
-        print('А может быть все-таки введем целое число от 1 до 100?')
+        print(f'А может быть все-таки введем целое число от 1 до {rng_numbers}?')
+        continue
     if num < n:
+        cnt += 1
         print('Ваше число меньше загаданного, попробуйте еще разок')
     elif num > n:
+        cnt += 1
         print('Ваше число больше загаданного, попробуте еще разок')
     elif num == n:
-        print(f'Поздравляю! вы угадали число {n}')
-        break
+        cnt += 1
+        print(f'Поздравляю! вы угадали число {n} за {cnt} попыток')
+
+        replay = input('Если хотите сыграть еще раз, введите "д", если нет, введите "н": ')     # Переигровка
+        if replay == 'д':
+            rng_numbers = 100
+
+            rng = input('Хотите выбрать диапазон чисел угадайки? Если да, введите "д", если нет, введите "н": ')
+            if rng == 'д':
+                make_range()
+
+            n = randint(1, rng_numbers)
+            cnt = 0
+            continue
+        else:
+            break
 
 print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
